0b18d1e337234b18884c828efde8ba8d73cd01a9
Buhaienko-Kate <buguzia89@gmail.com>
2021-10-19 14:49:27 +0300

    added task1. task2 lesson2

---
+++ Lesson2/task2/src/store.js
@@ -0,0 +1,53 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+const RESET = 'COUNTER/RESET';
+
+export const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+export const reset = () => {
+  return {
+    type: RESET,
+  };
+};
+
+const initialState = {
+  history: [],
+};
+
+const counterReduser = (state = initialState, action) => {
+  const { history } = state;
+
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        history: [...history, '+1'],
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        history: history.concat(-1),
+      };
+    case RESET:
+      return {
+        ...state,
+        history: [],
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReduser);
+
+export default store;
