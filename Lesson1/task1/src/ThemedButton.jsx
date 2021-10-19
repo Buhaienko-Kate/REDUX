6e75954063f78bf6cbe2ab00340c22f097921f44
Buhaienko-Kate <buguzia89@gmail.com>
2021-10-19 11:23:37 +0300

    added lesson 1 task1

---
+++ Lesson1/task1/src/ThemedButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ThemeContext } from './themes-context.js';
+
+class ThemedButton extends React.Component {
+  render() {
+    // console.log(this.context);
+    return (
+      <button
+        {...this.props}
+        className="btn"
+        style={{
+          backgroundColor: this.context.background,
+          color: this.context.fontColor,
+        }}
+      />
+    );
+  }
+}
+
+ThemedButton.contextType = ThemeContext;
+
+export default ThemedButton;
