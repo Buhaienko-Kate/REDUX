8ca65d977a591856d0a1a45cf473c0bdb069ebf7
Buhaienko-Kate <buguzia89@gmail.com>
2021-10-19 11:50:58 +0300

    added task2 lesson1

---
+++ Lesson1/task2/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './Header';
+import { DataContext } from './data-context.js';
+
+class App extends React.Component {
+  state = {
+    userData: {
+      name: 'Nikola Tesla',
+      avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+    },
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <DataContext.Provider value={this.state.userData}>
+          <Header />
+        </DataContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Lesson1/task2/src/UserMenu.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { DataContext } from './data-context.js';
+
+class UserMenu extends React.Component {
+  render() {
+    // console.log(this.context);
+    return (
+      <div className="menu">
+        <span className="menu__greeting">{this.context.name}</span>
+        <img alt="User Avatar" src={this.context.avatar_url} className="menu__avatar" />
+      </div>
+    );
+  }
+}
+
+UserMenu.contextType = DataContext;
+
+export default UserMenu;
